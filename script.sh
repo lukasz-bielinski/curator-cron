37ad0979162d9f0faa83e4c01ec8888cb387692d
lukasz-bielinski <lukasz-bielinski@users.noreply.github.com>
2016-09-09 10:13:17 +0200

    delete old logstash indices

--- script.sh
+++ script.sh
@@ -4,3 +4,4 @@ echo ""
 echo "Indices older than $TTL days will be deleted"
 echo ""
 curator --host $ELASTICSEARCH_HOST --port 9200 delete indices --older-than $TTL  --time-unit days --timestring '%Y-%m-%d'  2>&1
+curator --host $ELASTICSEARCH_HOST --port 9200 delete indices --older-than $TTL  --time-unit days --timestring '%Y.%m.%d'  2>&1
