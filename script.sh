e419ae1b8135068ce153d90a5039ce036936f707
Lukasz Bielinski <l.bielinski@oberthur.com>
2016-10-03 19:27:43 +0200

    reroute UNASSIGNED shards

--- script.sh
+++ script.sh
@@ -5,3 +5,32 @@ echo "Indices older than $TTL days will be deleted"
 echo ""
 curator --host $ELASTICSEARCH_HOST --port 9200 delete indices --older-than $TTL  --time-unit days --timestring '%Y-%m-%d'  2>&1
 curator --host $ELASTICSEARCH_HOST --port 9200 delete indices --older-than $TTL  --time-unit days --timestring '%Y.%m.%d'  2>&1
+
+
+
+
+##rerouting UNASSIGNED shards
+dataPodList=$(curl -XGET http://$ELASTICSEARCH_HOST:9200/_cat/nodes |grep ' d ' | awk '{print $8}')
+for NODE in $dataPodList
+  do
+       echo ""
+       echo "rerouting shards on node $NODE"
+       echo ""
+       IFS=$'\n'
+       for line in $(curl -s $ELASTICSEARCH_HOST:9200/_cat/shards | fgrep UNASSIGNED); do
+         INDEX=$(echo $line | (awk '{print $1}'))
+         SHARD=$(echo $line | (awk '{print $2}'))
+         curl -XPOST $ELASTICSEARCH_HOST:9200/_cluster/reroute -d '{
+            "commands": [
+               {
+                   "allocate": {
+                       "index": "'$INDEX'",
+                       "shard": '$SHARD',
+                       "node": "'$NODE'",
+                       "allow_primary": true
+                 }
+               }
+           ]
+         }'
+       done
+  done
