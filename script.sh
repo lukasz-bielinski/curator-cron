8fac2ab66b2b1a2614a90d31242d002075deffcc
Lukasz Bielinski <l.bielinski@oberthur.com>
2016-10-03 23:20:06 +0200

    modified:   script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ echo "Elasticsearch host: $ELASTICSEARCH_HOST"
 echo ""
 echo "Indices older than $TTL days will be deleted"
 echo ""
-curator --host $ELASTICSEARCH_HOST --port 9200 delete indices --older-than $TTL  --time-unit days --timestring '%Y-%m-%d'  | jq .
-curator --host $ELASTICSEARCH_HOST --port 9200 delete indices --older-than $TTL  --time-unit days --timestring '%Y.%m.%d'  | jq .
+curator --host $ELASTICSEARCH_HOST --port 9200 delete indices --older-than $TTL  --time-unit days --timestring '%Y-%m-%d'  2>&1
+curator --host $ELASTICSEARCH_HOST --port 9200 delete indices --older-than $TTL  --time-unit days --timestring '%Y.%m.%d'  2>&1
 
 
 
