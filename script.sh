ba6897452c9836e1b388a5931e1be3743bc220b2
Lukasz Bielinski <l.bielinski@oberthur.com>
2016-10-07 02:37:51 +0200

    modified:   script.sh

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ curator --logformat logstash --host $ELASTICSEARCH_HOST --port 9200 delete indic
 
 
 ##rerouting UNASSIGNED shards
-declare -a dataPodList=("$(curl -XGET http://$ELASTICSEARCH_HOST:9200/_cat/nodes |grep ' d ' | awk '{b=$8" "$9" "$10" "$11;   print b}')"")
+declare -a dataPodList=("$(curl -XGET http://$ELASTICSEARCH_HOST:9200/_cat/nodes |grep ' d ' | awk '{b=$8" "$9" "$10" "$11;   print b}')")
 for NODE in "${dataPodList[@]}"
   do
        echo ""
